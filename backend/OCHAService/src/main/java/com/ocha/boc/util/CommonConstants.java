8c82b916818623a4f4874ca0bc56280a25ea9006
Chinh Nguyen <chinh_nguyen.c>
2019-06-22 22:11:54 +0700

    [ADDED] CommonConstants file

---
+++ backend/OCHAService/src/main/java/com/ocha/boc/util/CommonConstants.java
@@ -0,0 +1,13 @@
+package com.ocha.boc.util;
+
+public class CommonConstants {
+
+    /**
+     * The FAILED status of the response object
+     */
+    public static final String STR_FAIL_STATUS = "FAILED";
+    /**
+     * The SUCCESS status of the response object
+     */
+    public static final String STR_SUCCESS_STATUS = "SUCCESS";
+}
