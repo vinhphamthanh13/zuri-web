0b790ad28a24900e9bab5eb9112e901790800eee
Chinh Nguyen <chinh_nguyen.c>
2019-06-23 10:13:07 +0700

    [UPDATED] Add new services

--- backend/OCHAService/src/main/java/com/ocha/boc/controllers/UserController.java
+++ backend/OCHAService/src/main/java/com/ocha/boc/controllers/UserController.java
@@ -1,14 +1,15 @@
 package com.ocha.boc.controllers;
 
+import com.ocha.boc.entity.User;
 import com.ocha.boc.request.UserLoginRequest;
+import com.ocha.boc.request.UserUpdateRequest;
 import com.ocha.boc.response.UserLoginResponse;
+import com.ocha.boc.response.UserUpdateResponse;
 import com.ocha.boc.services.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -17,9 +18,34 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+
     @PostMapping("/login")
-    public ResponseEntity<UserLoginResponse> checkLogin(@RequestBody UserLoginRequest request){
+    public ResponseEntity<UserLoginResponse> checkLogin(@RequestBody UserLoginRequest request) {
         UserLoginResponse response = userService.checkLogin(request);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestParam String phone) {
+        User user = userService.regiser(phone);
+        if (user == null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update-information")
+    public ResponseEntity<UserUpdateResponse> updateUserInformation(@RequestBody UserUpdateRequest request) {
+        UserUpdateResponse response = userService.updateUserInformation(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/deactive/{userId}")
+    public ResponseEntity deActiveUser(@PathVariable(name = "userId") String userId) {
+        boolean isDeActive = userService.deActiveUser(userId);
+        if (isDeActive) {
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.EXPECTATION_FAILED);
     }
 }
--- backend/OCHAService/src/main/java/com/ocha/boc/services/impl/UserService.java
+++ backend/OCHAService/src/main/java/com/ocha/boc/services/impl/UserService.java
@@ -3,36 +3,125 @@ package com.ocha.boc.services.impl;
 import com.ocha.boc.entity.User;
 import com.ocha.boc.repository.UserRepository;
 import com.ocha.boc.request.UserLoginRequest;
+import com.ocha.boc.request.UserUpdateRequest;
 import com.ocha.boc.response.UserLoginResponse;
+import com.ocha.boc.response.UserUpdateResponse;
 import com.ocha.boc.util.CommonConstants;
+import com.ocha.boc.util.DateUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
+
+    private static final String CURRENT_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS+07:00";
 
     @Autowired
     private UserRepository userRepository;
 
-    public UserLoginResponse checkLogin(UserLoginRequest request){
+    public UserLoginResponse checkLogin(UserLoginRequest request) {
         UserLoginResponse response = new UserLoginResponse();
-        if(!StringUtils.isNotEmpty(request.getPhone())){
+        if (!StringUtils.isNotEmpty(request.getPhone())) {
             response.setSuccess(Boolean.FALSE);
             response.setMessage(CommonConstants.STR_FAIL_STATUS);
             response.setObjectId(request.getPhone());
-        }else{
-            User user = userRepository.findByPhone(request.getPhone());
-            if(user == null){
+        } else {
+            User user = userRepository.findUserByPhone(request.getPhone());
+            if (user == null) {
                 response.setSuccess(Boolean.FALSE);
                 response.setMessage(CommonConstants.STR_FAIL_STATUS);
                 response.setObjectId(request.getPhone());
-            }else{
+            } else {
                 response.setSuccess(Boolean.TRUE);
                 response.setMessage(CommonConstants.STR_SUCCESS_STATUS);
                 response.setObjectId(request.getPhone());
+                response.setObject(user);
             }
         }
         return response;
     }
+
+
+    public User regiser(String phone) {
+        User user = null;
+        try {
+            if (StringUtils.isNotEmpty(phone)) {
+                user = userRepository.findUserByPhone(phone);
+                if (user != null) {
+                    log.error("Phone number is existed. Cannot register new account");
+                } else {
+                    user = new User();
+                    user.setPhone(phone);
+                    String currentDate = DateUtils.getCurrentDate();
+                    if (StringUtils.isNotEmpty(currentDate)) {
+                        long openDate = DateUtils.stringDateToLong(currentDate, null, CURRENT_DATE_FORMAT);
+                        if (openDate != 0) {
+                            user.setOpenDate(openDate);
+                        } else {
+                            log.error("Error when convert string current date to long date number");
+                        }
+                    } else {
+                        log.error("Cannot set openDate");
+                    }
+                    user.setActive(Boolean.TRUE);
+                    userRepository.save(user);
+                }
+            }
+        } catch (Exception e) {
+            log.error("Cannot register new user with phone number " + phone + ". Error: " + e);
+        }
+        return user;
+    }
+
+    public UserUpdateResponse updateUserInformation(UserUpdateRequest request) {
+        UserUpdateResponse response = new UserUpdateResponse();
+        try {
+            User user = checkUserExisted(request.getUserId());
+            if (user == null) {
+                response.setSuccess(Boolean.FALSE);
+                response.setMessage("User is not existed. Cannot update user information");
+                return response;
+            }
+            user.setEmail(request.getEmail());
+            user.setFirstName(request.getFirstName());
+            user.setLastName(request.getLastName());
+            user.setPhoto(request.getPhoto());
+            userRepository.save(user);
+            response.setSuccess(Boolean.TRUE);
+            response.setMessage("Update user successful");
+            response.setObjectId(request.getUserId());
+            response.setObject(user);
+        } catch (Exception e) {
+            log.error("Error when updateUserInformation: ", e);
+        }
+        return response;
+    }
+
+    public boolean deActiveUser(String userId) {
+        boolean isDeActive = false;
+        try {
+            User user = userRepository.findUserById(userId);
+            if (user != null) {
+                user.setActive(Boolean.FALSE);
+                isDeActive = true;
+            }
+        } catch (Exception e) {
+            log.error("Error when deActiveUser: ", e);
+        }
+        return isDeActive;
+    }
+
+    private User checkUserExisted(String id) {
+        User user = null;
+        try {
+            user = userRepository.findUserById(id);
+        } catch (Exception e) {
+            log.error("Error when checkUserExisted: ", e);
+        }
+        return user;
+    }
+
 }
