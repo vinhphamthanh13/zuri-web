6f17b9243e1e22ba22ac639bbcb99c50c129538e
Chinh Nguyen <chinh_nguyen.c>
2019-06-23 10:12:33 +0700

    [UPDATED] findUserById

--- backend/OCHAService/src/main/java/com/ocha/boc/repository/UserRepository.java
+++ backend/OCHAService/src/main/java/com/ocha/boc/repository/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
 
-    User findByPhone(String phone);
+    User findUserByPhone(String phone);
+    User findUserById(String id);
 }
