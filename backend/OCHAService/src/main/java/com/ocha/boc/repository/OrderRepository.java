d9335d41040476c82b916d0b6f53f0ef949b0ced
Chinh Nguyen <chinh_nguyen.c>
2019-07-03 18:09:00 +0700

    [UPDATE] method "findListOrderByCreateDate"

--- backend/OCHAService/src/main/java/com/ocha/boc/repository/OrderRepository.java
+++ backend/OCHAService/src/main/java/com/ocha/boc/repository/OrderRepository.java
@@ -2,6 +2,12 @@ package com.ocha.boc.repository;
 
 import com.ocha.boc.entity.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends MongoRepository<Order, String > {
+
+    @Query(value = "{'createDate': ?0}")
+    List<Order> findListOrderByCreateDate(String date);
 }
