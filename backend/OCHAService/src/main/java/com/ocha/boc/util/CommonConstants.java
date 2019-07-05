9fbd1f7e4ff492386b2ccc6fabeca64598e1ffd5
Chinh Nguyen <chinh_nguyen.c>
2019-07-06 00:20:58 +0700

    [UPDATED] error message on case "Khuyen Mai"

--- backend/OCHAService/src/main/java/com/ocha/boc/util/CommonConstants.java
+++ backend/OCHAService/src/main/java/com/ocha/boc/util/CommonConstants.java
@@ -10,4 +10,14 @@ public class CommonConstants {
      * The SUCCESS status of the response object
      */
     public static final String STR_SUCCESS_STATUS = "SUCCESS";
+
+    public static final String CREATE_NEW_KHUYEN_MAI_FAIL               = "Không thể tạo mới mã khuyến mãi. Lỗi hệ thống";
+
+    public static final String GET_KHUYEN_MAI_BY_KHUYEN_MAI_ID_FAIL     = "Không tìm thấy khuyến mãi theo yêu cầu. Lỗi hệ thống";
+
+    public static final String GET_ALL_KHUYEN_MAI_FAIL                  = "Không tìm thấy thông tin khuyến mãi. Lỗi hệ thống";
+
+    public static final String DELETE_KHUYEN_MAI_BY_KHUYEN_MAI_ID_FAIL  = "Không thể xóa mã khuyến mãi này. Lỗi hệ thống";
+
+    public static final String UPDATE_KHUYEN_MAI_FAIL                   = "Không thể cập nhật cho mã khuyến mãi này. Lỗi hệ thống";
 }
