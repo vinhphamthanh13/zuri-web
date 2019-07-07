4ccbd18ded9ef229a471d7eb1bcb54b4bbd64586
Chinh Nguyen <chinh_nguyen.c>
2019-07-07 10:09:16 +0700

    [UPDATED] calculate discount price

--- backend/OCHAService/src/main/java/com/ocha/boc/services/impl/OrderService.java
+++ backend/OCHAService/src/main/java/com/ocha/boc/services/impl/OrderService.java
@@ -104,7 +104,6 @@ public class OrderService {
                 matHangBanChay.setDiscountPrice(BigDecimal.valueOf(discountPrice));
             }
         }
-        matHangBanChay.setDiscountPrice(matHangTieuThu.getDiscountPrice());
         return matHangBanChay;
     }
 
