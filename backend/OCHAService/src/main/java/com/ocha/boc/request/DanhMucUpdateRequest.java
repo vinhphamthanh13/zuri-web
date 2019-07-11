cfdcf5e65ce8ce87ce069d1cc936f635dcc327c5
Chinh Nguyen <chinh_nguyen.c>
2019-07-11 15:25:01 +0700

    [ADDED] DanhMucUpdateRequest object

---
+++ backend/OCHAService/src/main/java/com/ocha/boc/request/DanhMucUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.ocha.boc.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+public class DanhMucUpdateRequest implements Serializable {
+
+    private String abbreviations;
+
+    private String name;
+
+    private String danhMucId;
+}
