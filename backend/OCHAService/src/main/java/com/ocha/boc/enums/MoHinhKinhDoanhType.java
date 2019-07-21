9b5aade9c2e855a04b4b68bfa909cd354d9996ec
Chính Nguyễn Công <chinhncse@gmail.com>
2019-07-22 01:00:11 +0700

    [UPDATE] "MoHinhKinhDoanhType" enum class which will be used on "CuaHang" table

---
+++ backend/OCHAService/src/main/java/com/ocha/boc/enums/MoHinhKinhDoanhType.java
@@ -0,0 +1,11 @@
+package com.ocha.boc.enums;
+
+public enum MoHinhKinhDoanhType {
+    QUAN_CAO_CAP("Quán cao cấp");
+
+    public final String label;
+
+    private MoHinhKinhDoanhType(String label){
+        this.label = label;
+    }
+}
