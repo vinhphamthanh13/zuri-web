d62529f187333cd0d859214e31f3f2d6ce8818ca
Chính Nguyễn Công <chinhncse@gmail.com>
2019-07-22 19:38:28 +0700

    [UPDATE] "CuaHangUpdateRequest" class object

---
+++ backend/OCHAService/src/main/java/com/ocha/boc/request/CuaHangUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.ocha.boc.request;
+
+import com.ocha.boc.enums.MoHinhKinhDoanhType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+public class CuaHangUpdateRequest implements Serializable {
+
+    private String id;
+
+    private MoHinhKinhDoanhType moHinhKinhDoanhType;
+
+    private String address;
+
+    private String managerEmail;
+}
