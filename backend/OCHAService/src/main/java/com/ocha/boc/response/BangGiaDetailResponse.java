f5ee44bf15edcbd0c8dcd0360c33590ed25d2b35
Chinh Nguyen <chinhncse@gmail.com>
2019-07-24 14:48:26 +0700

    [UPDATE] Getter, Setter, ToString annotation

--- backend/OCHAService/src/main/java/com/ocha/boc/response/BangGiaDetailResponse.java
+++ backend/OCHAService/src/main/java/com/ocha/boc/response/BangGiaDetailResponse.java
@@ -2,6 +2,12 @@ package com.ocha.boc.response;
 
 import com.ocha.boc.base.AbstractResponse;
 import com.ocha.boc.dto.BangGiaDetailDTO;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class BangGiaDetailResponse extends AbstractResponse<String, BangGiaDetailDTO> {
 }
