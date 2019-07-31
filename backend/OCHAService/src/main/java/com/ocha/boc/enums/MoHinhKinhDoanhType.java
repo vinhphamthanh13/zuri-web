8b25799f554296847c47043c56603368499639e6
Chinh Nguyen <chinhncse@gmail.com>
2019-07-31 15:57:05 +0700

    [UPDATE] add more properties "MoHinhKinhDoanhType"

--- backend/OCHAService/src/main/java/com/ocha/boc/enums/MoHinhKinhDoanhType.java
+++ backend/OCHAService/src/main/java/com/ocha/boc/enums/MoHinhKinhDoanhType.java
@@ -1,7 +1,19 @@
 package com.ocha.boc.enums;
 
 public enum MoHinhKinhDoanhType {
-    QUAN_CAO_CAP("Quán cao cấp");
+    QUÁN_CAO_CẤP("Quán cao cấp"),
+
+    QUÁN_BÌNH_DÂN("Quán bình dân"),
+
+    QUÁN_ĂN_NHANH("Quán ăn nhanh"),
+
+    QUÁN_ĂN_TỰ_CHỌN("Quán ăn tự chọn"),
+
+    NHƯỢNG_QUYỀN("Nhượng quyền"),
+
+    PHỤC_VỤ_NHANH_MANG_ĐI("Phục vụ nhanh mang đi"),
+
+    KHÁC("Khác");
 
     public final String label;
 
