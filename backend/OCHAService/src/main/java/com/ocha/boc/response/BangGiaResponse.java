8f86de27b0cbd5b1f25fe8dbf69af3d87b12c8ca
Chinh Nguyen <chinhncse@gmail.com>
2019-07-31 15:54:52 +0700

    [DELETE] unused class object

--- backend/OCHAService/src/main/java/com/ocha/boc/response/BangGiaResponse.java
+++
@@ -1,9 +0,0 @@
-package com.ocha.boc.response;
-
-import com.ocha.boc.base.AbstractResponse;
-import com.ocha.boc.dto.BangGiaDTO;
-
-import java.io.Serializable;
-
-public class BangGiaResponse extends AbstractResponse<String , BangGiaDTO> implements Serializable {
-}
