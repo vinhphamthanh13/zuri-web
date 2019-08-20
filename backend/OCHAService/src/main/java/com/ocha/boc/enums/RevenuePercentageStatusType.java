e64de22a7de83976dc16281cdc7617c4b3411d19
Chính Nguyễn Công <chinhncse@gmail.com>
2019-08-20 08:30:49 +0700

    [ADD] RevenuePercentageStatusType class object which will be used in "Bao Cao Session"

---
+++ backend/OCHAService/src/main/java/com/ocha/boc/enums/RevenuePercentageStatusType.java
@@ -0,0 +1,19 @@
+package com.ocha.boc.enums;
+
+
+public enum RevenuePercentageStatusType {
+
+    INCREASE("INCREASE"),
+
+    DECREASE("DECREASE"),
+
+    INCREASE_INFINITY("INCREASE_INFINITY"),
+
+    DECREASE_INFINITY("DECREASE_INFINITY");
+
+    public final String label;
+
+    private RevenuePercentageStatusType(String label){
+        this.label = label;
+    }
+}
