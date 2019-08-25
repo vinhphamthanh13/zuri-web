15be62abdee21494ed585eab8574fc0e2c4ef293
Chính Nguyễn Công <chinhncse@gmail.com>
2019-08-26 00:06:38 +0700

    [ADDED] PageRequest Object: be used when call findAll()

---
+++ backend/OCHAService/src/main/java/com/ocha/boc/request/PageRequest.java
@@ -0,0 +1,15 @@
+package com.ocha.boc.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class PageRequest {
+    private String cuaHangId;
+    private Integer page;
+    private Integer size;
+    private String sort;
+}
