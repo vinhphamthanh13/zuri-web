64e9b129ef4e0a108adba0b43b5cdb172b5c130c
Chinh Nguyen <chinhncse@gmail.com>
2019-08-27 15:30:16 +0700

    [UPDATE] Bao Cao Session: Refactor Code

--- backend/OCHAService/src/main/java/com/ocha/boc/repository/OrderRepository.java
+++ backend/OCHAService/src/main/java/com/ocha/boc/repository/OrderRepository.java
@@ -2,7 +2,6 @@ package com.ocha.boc.repository;
 
 import com.ocha.boc.entity.Order;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
