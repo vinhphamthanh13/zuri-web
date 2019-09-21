0a7d33f526b876e863a7b8004860eb88770b65f6
Chính Nguyễn Công <chinhncse@gmail.com>
2019-09-21 21:57:58 +0700

    [UPDATED] Remove unused classes

--- backend/OCHAService/src/main/java/com/ocha/boc/response/MatHangResponse.java
+++
@@ -1,9 +0,0 @@
-package com.ocha.boc.response;
-
-import com.ocha.boc.base.AbstractResponse;
-import com.ocha.boc.dto.MathangDTO;
-
-import java.io.Serializable;
-
-public class MatHangResponse extends AbstractResponse<String, MathangDTO> implements Serializable {
-}
