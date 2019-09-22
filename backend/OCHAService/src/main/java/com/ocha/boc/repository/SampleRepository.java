86a361168ac71b80d893c305a57496d8b280ddf1
Chính Nguyễn Công <chinhncse@gmail.com>
2019-09-23 00:19:20 +0700

    [UPDATED] remove unused classes

--- backend/OCHAService/src/main/java/com/ocha/boc/repository/SampleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ocha.boc.repository;
-
-import com.ocha.boc.entity.SampleEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SampleRepository extends MongoRepository<SampleEntity, String>  {
-}
