0f86794ccdf3f1c623ca2ad64835523a5e6a3704
Chính Nguyễn Công <chinhncse@gmail.com>
2019-09-23 23:38:22 +0700

    [UPDATED] Copy Printer Session: refactor code

--- backend/OCHAService/src/main/java/com/ocha/boc/entity/CopyPrinter.java
+++ backend/OCHAService/src/main/java/com/ocha/boc/entity/CopyPrinter.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 @Setter
 @ToString
 @Builder
+@Document(collection = CopyPrinter.COLLECTION_NAME)
 public class CopyPrinter extends AbstractEntity {
 
     public static final String COLLECTION_NAME = "copyprinter";
